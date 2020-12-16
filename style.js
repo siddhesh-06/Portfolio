094cf0c0739770dc1ae1920f2088b705da1a904a
siddhesh-06 <61205098+siddhesh-06@users.noreply.github.com>
2020-12-16 21:10:50 +0530

    update all animation with aos

--- style.js
+++ style.js
@@ -1,16 +1,2 @@
-anime.timeline({loop: false})
-  .add({
-    targets: '.ml15 .word',
-    scale: [14,1],
-    opacity: [0,1],
-    easing: "easeOutCirc",
-    duration: 1200,
-    delay: (el, i) => 800 * i
-  }).add({
-    targets: '.ml15',
-    opacity: [0,1],
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 100
-  });
+
 
